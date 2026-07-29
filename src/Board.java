896bd115c79ed814c05ac578af06159612f64e4c
sylvestrisaint <opensystechs@gmail.com>
2026-07-28 21:53:01 -0400

    updated scoring logic

--- src/Board.java
+++ src/Board.java
@@ -11,26 +11,89 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.text.Font;
 import javafx.util.Duration;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board {
     private Game gamePlay;
-    private GridPane pitGrid;
     private Pit[][] pits = new Pit[4][8];
+    private final List<int[]> playerOnePath;
+    private final List<int[]> playerTwoPath;
     private boolean isSowInProgress = false;
+    private boolean isPitClicked = false;
+    private Label playerOneCountLabel;
+    private Label playerTwoCountLabel;
+    private int playerOneBeads;
+    private int playerTwoBeads;
     private int currentPlayer = 1;
 
     public Board(Game gamePlay, BorderPane gamePane) {
         this.gamePlay = gamePlay;
+        this.playerOneBeads = 32;
+        this.playerTwoBeads = 32;
         this.setupBoardWithPits(gamePane);
+        this.playerOnePath = this.buildPath(0, 1);
+        this.playerTwoPath = this.buildPath(2, 3);
+    }
+
+    private void setupBoardWithPits(BorderPane gamePane) {
+        Image image = new Image(this.getClass().getResourceAsStream("/resources/board.png"));
+        ImageView boardImageView = new ImageView(image);
+        boardImageView.setFitWidth(Constants.BOARD_WIDTH);
+        boardImageView.setFitHeight(Constants.BOARD_HEIGHT);
+        boardImageView.setPreserveRatio(true);
+
+        GridPane pitGrid = new GridPane();
+        pitGrid.setPrefSize(Constants.BOARD_WIDTH, Constants.BOARD_HEIGHT);
+        pitGrid.setMaxSize(Constants.BOARD_WIDTH, Constants.BOARD_HEIGHT);
+
+        for(int row = 0; row < 4; row++) {
+            for (int col = 0; col < 8; col++) {
+                Pit pit = new Pit(row, col, 2);
+                pit.getView().setPrefSize(Constants.BOARD_WIDTH / 8.0, Constants.BOARD_HEIGHT / 4.0);
+                pit.setOnPitClicked(this::handlePitClicked);
+
+                pitGrid.add(pit.getView(), col, row);
+
+                this.pits[row][col] = pit;
+            }
+        }
+
+        StackPane boardStack = new StackPane();
+        boardStack.getChildren().addAll(boardImageView, pitGrid);
+
+        Font customFont = Font.loadFont(this.getClass().
+                getResourceAsStream("fonts/noot-regular.ttf"), 24);
+        if (customFont == null) { customFont = Font.getDefault();}
+
+        this.playerOneCountLabel = new Label(String.valueOf(this.playerOneBeads));
+        this.playerOneCountLabel.setFont(customFont);
+        this.playerOneCountLabel.getStyleClass().add("label-gunn");
+        this.playerTwoCountLabel = new Label(String.valueOf(this.playerTwoBeads));
+        this.playerTwoCountLabel.setFont(customFont);
+        this.playerTwoCountLabel.getStyleClass().add("label-gunn");
+
+        HBox boardRow = new HBox(20.0);
+        boardRow.setAlignment(Pos.CENTER);
+        boardRow.getChildren().addAll(this.playerOneCountLabel, boardStack, this.playerTwoCountLabel);
+
+        BorderPane.setAlignment(boardRow, Pos.CENTER);
+        gamePane.setCenter(boardRow);
     }
 
     public void sowFromAnimated(int startRow, int startCol, Duration stepDelay, Runnable onComplete) {
+        List<int[]> path = this.getPathForRow(startRow);
+        int index = this.indexOf(path, startRow, startCol);
+
         int scoredBeads = 0;
 
-        if (startRow == 1 || startRow == 2) {
+        if (!isPitClicked && (startRow == 1 || startRow == 2)) {
             Pit oppPitOne;
             Pit oppPitTwo;
+            boolean isPlayerOne = false;
 
             if (this.currentPlayer == 1) {
+                isPlayerOne = true;
                 oppPitOne = this.pits[startRow + 1][startCol];
                 oppPitTwo = this.pits[startRow + 1][startCol];
             } else {
@@ -45,7 +108,10 @@ public class Board {
                 scoredBeads = oppPitTwo.getBeadCount();
                 oppPitTwo.setBeadCount(0);
             }
+            this.updateBeadCounts(isPlayerOne, scoredBeads);
         }
+
+        this.isPitClicked = false;
 
         Pit start = this.pits[startRow][startCol];
         start.setBeadCount(start.getBeadCount() + scoredBeads);
@@ -54,17 +120,13 @@ public class Board {
         start.flashRemoved(0);
 
         SequentialTransition sequence = new SequentialTransition();
-        int row = startRow, col = startCol;
         Pit[] lastPitHolder = new Pit[1];
 
         for (int i = 0; i < beadsToSow; i++) {
-            col++;
-            if (col >= 8) {
-                col = 0;
-                row = (row + 1) % 4;
-            }
+            index = (index + 1) % path.size();
+            int[] coord = path.get(index);
 
-            final Pit next = this.pits[row][col];
+            final Pit next = this.pits[coord[0]][coord[1]];
             PauseTransition step = new PauseTransition(stepDelay);
             step.setOnFinished(e -> {
                 next.flashAdded(next.getBeadCount() + 1);
@@ -76,7 +138,6 @@ public class Board {
 
         sequence.setOnFinished(e -> {
             if (onComplete != null) {
-//                onComplete.run();
                 if (lastPitHolder[0].getBeadCount() != 1) {
                     sowFromAnimated(lastPitHolder[0].getRow(), lastPitHolder[0].getCol(), Duration.millis(150), ()-> {
                         this.isSowInProgress = false;
@@ -91,52 +152,8 @@ public class Board {
         sequence.play();
     }
 
-    private void setupBoardWithPits(BorderPane gamePane) {
-        Image image = new Image(this.getClass().getResourceAsStream("/resources/board.png"));
-        ImageView boardImageView = new ImageView(image);
-        boardImageView.setFitWidth(Constants.BOARD_WIDTH);
-        boardImageView.setFitHeight(Constants.BOARD_HEIGHT);
-        boardImageView.setPreserveRatio(true);
-
-        this.pitGrid = new GridPane();
-        this.pitGrid.setPrefSize(Constants.BOARD_WIDTH, Constants.BOARD_HEIGHT);
-        this.pitGrid.setMaxSize(Constants.BOARD_WIDTH, Constants.BOARD_HEIGHT);
-
-        for(int row = 0; row < 4; row++) {
-            for (int col = 0; col < 8; col++) {
-                Pit pit = new Pit(row, col, 2);
-                pit.getView().setPrefSize(Constants.BOARD_WIDTH / 8.0, Constants.BOARD_HEIGHT / 4.0);
-                pit.setOnPitClicked(this::handlePitClicked);
-
-                this.pitGrid.add(pit.getView(), col, row);
-
-                this.pits[row][col] = pit;
-            }
-        }
-
-        StackPane boardStack = new StackPane();
-        boardStack.getChildren().addAll(boardImageView, this.pitGrid);
-
-        Font customFont = Font.loadFont(this.getClass().
-                getResourceAsStream("fonts/noot-regular.ttf"), 24);
-        if (customFont == null) { customFont = Font.getDefault();}
-
-        Label leftCount = new Label("32");
-        leftCount.setFont(customFont);
-        leftCount.getStyleClass().add("label-gunn");
-        Label rightCount = new Label("32");
-        rightCount.setFont(customFont);
-        rightCount.getStyleClass().add("label-gunn");
-
-        HBox boardRow = new HBox(20.0);
-        boardRow.setAlignment(Pos.CENTER);
-        boardRow.getChildren().addAll(leftCount, boardStack, rightCount);
-
-        BorderPane.setAlignment(boardRow, Pos.CENTER);
-        gamePane.setCenter(boardRow);
-    }
-
     private void handlePitClicked(Pit pit) {
+        this.isPitClicked = true;
         if (!gamePlay.gameIsActive() || this.isSowInProgress) {
             return;
         }
@@ -151,21 +168,8 @@ public class Board {
         this.sowFromAnimated(pit.getRow(), pit.getCol(), Duration.millis(150), () -> {
             this.isSowInProgress = false;
             this.switchTurn();
-//            this.checkCaptureRules(pit.getRow(), pit.getCol());
         });
     }
-
-//    private void checkCaptureRules(int lastRow, int lastCol) {
-//        Pit pit = new Pit(lastRow, lastCol);
-//        if (beadCount == 1) {
-//            this.switchTurn();
-//        } else {
-//            this.sowFromAnimated(lastRow, lastCol, Duration.millis(150), () -> {
-//                this.isSowInProgress = false;
-//                this.checkCaptureRules();
-//            });
-//        }
-//    }
 
     private boolean isValidMove(Pit pit) {
         if (pit.getBeadCount() == 0) {
@@ -184,5 +188,46 @@ public class Board {
     private void switchTurn() {
         this.currentPlayer = (this.currentPlayer == 1) ? 2 : 1;
         gamePlay.switchPlayer("PLAYER " + this.currentPlayer + " 'S TURN");
+    }
+
+    private void updateBeadCounts(boolean isPlayerOne, int count) {
+        if (isPlayerOne) {
+            this.playerOneBeads += count;
+            this.playerTwoBeads -= count;
+        } else {
+            this.playerOneBeads -= count;
+            this.playerTwoBeads += count;
+        }
+
+        this.playerOneCountLabel.setText(String.valueOf(this.playerOneBeads));
+        this.playerTwoCountLabel.setText(String.valueOf(this.playerTwoBeads));
+    }
+
+    private List<int[]> buildPath(int topRow, int bottomRow) {
+        List<int[]> path = new ArrayList<>();
+
+        for (int col = 7; col >= 0; col--) {
+            path.add(new int[]{topRow, col});
+        }
+
+        for (int col = 0; col < 8; col++) {
+            path.add(new int[] {bottomRow, col});
+        }
+
+        return path;
+    }
+
+    private List<int[]> getPathForRow(int row) {
+        return (row < 2) ? this.playerOnePath : this.playerTwoPath;
+    }
+
+    private int indexOf(List<int[]> path, int row, int col) {
+        for (int i = 0; i < path.size(); i++) {
+            if (path.get(i)[0] == row && path.get(i)[1] == col) {
+                return i;
+            }
+        }
+
+        throw new IllegalStateException("Pit (" + row + ", " + col + ") not found in path");
     }
 }
